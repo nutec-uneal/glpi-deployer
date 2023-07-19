45cba6391407e9a0f0ac464dcdb1f0ff93c5cf54
Ramon Almeida <ramongoncalves76@gmail.com>
2023-07-19 11:10:38 -0300

    feat: glpi job, add mult-container support

--- scripts/glpi-jobs/glpi-job-docker.sh
+++ scripts/glpi-jobs/glpi-job-docker.sh
@@ -7,20 +7,45 @@ if [[ -z $CONTAINER_NAME ]]; then
     exit 1
 fi
 
-status=$(docker ps --filter "status=running" | grep $CONTAINER_NAME)
+containers=($(docker ps --filter "status=running" 2> /dev/null | grep -Po "$CONTAINER_NAME\$" | tr "\n" " "))
 
-if [[ -z $status ]]; then
-    echo "[$(date)] [ERROR] - Container not running." >&2
+if [[ ${#containers[@]} -le 0 ]]; then
+    echo "[$(date)] [ERROR] - Container(s) not running. No match for '$CONTAINER_NAME'" >&2
     
     exit 1
 fi
 
-result=$(docker exec -it $CONTAINER_NAME php /var/www/html/front/cron.php)
+log_reatime=
+failure_counter=0
 
-if [[ ! -z $result ]]; then
-    echo "[$(date)] [ERROR] - Command execution error, $result" >&2
+
+exec_command() {
+    result=$(docker exec -it $actual_container php /var/www/html/front/cron.php 2>&1)
+    
+    if [[ ! -z $result ]]; then
+        ((failure_counter++))
+        log_reatime+=$(echo "'Command execution error in $actual_container, $result',")
+    fi
+}
+
+
+containers_array_size=${#containers[@]}
+
+if [[ ! -z $FIRST_MODE_ON ]]; then
+    actual_container=${containers[0]}
+    containers_array_size=1
+    exec_command
+else
+    for name in ${containers[@]}; do
+        actual_container=$name
+        exec_command
+    done
+fi
+
+if [[ $containers_array_size -eq $failure_counter ]]; then
+    echo "[$(date)] [ERROR] - [ $log_reatime ]" >&2
     
     exit 1
 fi
 
-echo "[$(date)] [OK] - Successfully executed."
+echo "[$(date)] [OK] - Successfully executed. Details: [ $log_reatime ]"
